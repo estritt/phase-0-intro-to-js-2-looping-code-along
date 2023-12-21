d45ec3ca8c73c3724dd5d45cda7ed6633e139255
Genevieve Strittmatter <eastrittmatter@gmail.com>
2023-12-21 17:28:28 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const greetings = [];
+    for (let i = 0; i < names.length; i++) {
+        greetings.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return greetings;
+}
+
+function countDown(n) {
+    while(n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
